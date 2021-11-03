fb39e68a0efacfaf01107524b679ff6d90ebe0ed
idris agboola <agboolaisholaidreez@gmail.com>
2021-11-03 16:53:51 +0100

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,18 @@
-import * as React from "react";
-import Head from "next/head";
-import { AppProps } from "next/app";
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import theme from "../styles/theme";
-import createEmotionCache from "../../src/lib/createEmotionCache";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../styles/theme';
+import createEmotionCache from '../lib/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
-}
-
-const App = (props: MyAppProps) => {
+export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -29,6 +26,10 @@ const App = (props: MyAppProps) => {
       </ThemeProvider>
     </CacheProvider>
   );
-};
+}
 
-export default App;
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
